855f34db0543a599b3eec903e62a754b938241d0
Jesse Cravens <jesse.cravens@frogdesign.com>
2013-03-10 11:57:28 -0500

    change db driver

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 //mongo uri
-app.set('mongodb-uri', 'mongodb://nodejitsu_jdcravens:okecgboe57biqe4jvkm6nl7pob@ds051947.mongolab.com:51947/nodejitsu_jdcravens_nodejitsudb1954665151' || process.env.MONGOHQ_URL || 'localhost/starter');
+app.set('mongodb-uri', 'mongodb://nodejitsu_jdcravens:lmjd49kc80bf1pcitjlt6hv3hh@ds051947.mongolab.com:51947/nodejitsu_jdcravens_nodejitsudb8260783110' || process.env.MONGOHQ_URL || 'localhost/starter');
 
 //setup mongoose
 app.db = mongoose.createConnection(app.get('mongodb-uri'));
